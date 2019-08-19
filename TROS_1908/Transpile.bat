050e6731562fb2a41b554d508faeacea8e0d4760
Anuvrat Singh <Anuvrat.singh@kdi.kongsberg.com>
2019-08-19 09:56:52 +0000

    dsdfs

---
+++ TROS_1908/Transpile.bat
@@ -0,0 +1,3 @@
+ECHO %1%
+CD %1%
+npm run tsc
--- spec.js
+++ spec.js
@@ -1,9 +1,31 @@
-describe('Protractor Demo App', function() {
-  it('should have a title', function() {
-	 browser.ignoreSynchronization=true;
-    browser.get('https://google.com');
+import { browser, element, by, By, protractor, ElementArrayFinder, ElementFinder } from "protractor";
 
-   browser.sleep(20000)
-	
+describe('Protractor Demo App', function() {
+    it('should have a title', function() {
+       browser.ignoreSynchronization=true;
+      browser.get('https://google.com');
+  
+     browser.sleep(20000)
+      
+    });
+    
+    it('Login to IMDB', function() {
+       browser.ignoreSynchronization=true;
+       browser.get('https://www.imdb.com');
+  
+       browser.sleep(20000)
+      
+    });
+    
+    it('Login to Facebook', function() {
+       browser.ignoreSynchronization=true;
+       browser.get('https://www.facebook.com/');
+       browser.sleep(20000)
+       element(by.xpath('//input[@type="email"]')).sendKeys('k.nandini95@gmail.com')
+       browser.sleep(1000)
+       element(by.id('pass')).sendKeys('random11')
+       element(by.xpath('//input[@value="Log In"]')).click()
+       expect(element(by.xpath('//a[text()="Forgotten password?"]')).isDisplayed()).toBe(false)
+    });
+    
   });
-});
