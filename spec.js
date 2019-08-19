ed62b4cac8b1d7d16c7ac28005b6d4560f1c2674
Anuvrat Singh <Anuvrat.singh@kdi.kongsberg.com>
2019-08-19 08:38:39 +0000

    asa

--- spec.js
+++ spec.js
@@ -1,5 +1,6 @@
 describe('Protractor Demo App', function() {
   it('should have a title', function() {
+	 browser.ignoreSynchronization=true;
     browser.get('https://google.com');
 
    browser.sleep(20000)
